07e04021fd95da1f9816687901e884545285cf96
mikestone <stonemj@gmail.com>
2012-07-10 12:29:10 -0700

    Detect the default to change/grab inside.

--- edit-utils.el
+++ edit-utils.el
@@ -4,10 +4,13 @@
     `(defun ,(intern (concat type "-inside")) (&optional type-or-ask)
        ,help
        (interactive)
-       (let ((type (or type-or-ask (read-string "Inside: "))))
+       (let* ((default-type (get-default-change-type))
+              (retrieved-type (or type-or-ask (read-string (concat "Inside (default " default-type "): "))))
+              (type (if (equal retrieved-type "") default-type retrieved-type)))
          (cond ((or (equal type "(") (equal type ")")) (,fn "(" ")"))
                ((or (equal type "{") (equal type "}")) (,fn "{" "}"))
                ((or (equal type "[") (equal type "]")) (,fn "[" "]"))
+               ((or (equal type "<%") (equal type "%>")) (,fn "<%" "%>"))
                ((equal type "\\") (,fn type type)) ; TODO: handle \\ properly
                ((equal type "\"") (,fn type type)) ; TODO: handle \" properly
                (t (,fn type type)))))))
@@ -17,6 +20,40 @@
 
 (def-inside "grab" "grab-between"
   "Grab inside a given type of character (supported types: parens, braces, brakets or any arbitrary string, with special parsing for forward slash and double quotes)")
+
+(defun get-default-change-type ()
+  "Retrieve the closest surrounding changeable type"
+  (let* ((types '(("(" ")")
+                  ("{" "}")
+                  ("[" "]")
+                  ("<%" "%>")
+                  ("\"" "\"")
+                  ("'" "'")))
+         (type-with-inside-lengths (mapcar (lambda (type-pair) (cons (get-inside-length type-pair) type-pair)) types))
+         (closest-type "")
+         (closest-type-size nil))
+    (dolist (type-details type-with-inside-lengths closest-type)
+      (let ((type (cadr type-details))
+            (inside-length (car type-details)))
+        (if (and inside-length (or (not closest-type-size) (< inside-length closest-type-size)))
+            (progn
+              (setq closest-type-size inside-length)
+              (setq closest-type type)))))))
+
+(defun get-inside-length (type-pair)
+  "Get the length from the current buffer"
+  (let* ((open-type (car type-pair))
+         (close-type (cadr type-pair))
+         (heterogeneous (not (equal open-type close-type)))
+         (open-backward-position (save-excursion (search-backward open-type nil t)))
+         (close-backward-position (if heterogeneous (save-excursion (search-backward close-type nil t))))
+         (open-forward-position (if heterogeneous (save-excursion (search-forward open-type nil t))))
+         (close-forward-position (save-excursion (search-forward close-type nil t))))
+    (if (or (not close-backward-position)
+            (> open-backward-position close-backward-position))
+        (if (or (not open-forward-position)
+                (< close-forward-position open-forward-position))
+            (- close-forward-position open-backward-position)))))
 
 (defun delete-between (start-string end-string)
   "Delete everything between the given start and end value from the current cursor"
