55f86350e62dbc999cb00e6208942e152d92b89c
mikestone <stonemj@gmail.com>
2012-11-13 17:06:33 -0800

    Allow usage of sh via the new function new-shell.

--- new-shell.el
+++ new-shell.el
@@ -1,7 +1,11 @@
+(defun new-shell (shell-name)
+  "Generate a new shell with the given name"
+  (let* ((new-buffer (generate-new-buffer shell-name)))
+    (set-window-buffer (selected-window) new-buffer)
+    (shell new-buffer)))
+
 (defun sh ()
   "Interactively generate a new shell with the given name"
   (interactive)
-  (let* ((shell-name (read-string "New shell name: "))
-         (new-buffer (generate-new-buffer shell-name)))
-    (set-window-buffer (selected-window) new-buffer)
-    (shell new-buffer)))
+  (let* ((shell-name (read-string "New shell name: ")))
+    (new-shell shell-name)))
