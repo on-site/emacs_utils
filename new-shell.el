a5e31d1774e4ed0e2b8159aeaa87c9aa25212b4b
Sriram Thaiyar <sthaiyar@on-site.com>
2012-11-27 10:28:52 -0800

    Add shell hacks to make it work like Bash up and down arrows

--- new-shell.el
+++ new-shell.el
@@ -9,3 +9,20 @@
   (interactive)
   (let* ((shell-name (read-string "New shell name: ")))
     (new-shell shell-name)))
+
+(defun my-shell-up (&optional arg)
+  (interactive "p")
+  (if (eobp)
+      (comint-previous-input arg)
+    (previous-line arg)))
+
+(defun my-shell-down (&optional arg)
+  (interactive "p")
+  (if (eobp)
+      (comint-next-input arg)
+    (next-line arg)))
+
+(add-hook 'shell-mode-hook
+	  (lambda ()
+	    (define-key shell-mode-map (kbd "<up>") 'my-shell-up)
+	    (define-key shell-mode-map (kbd "<down>") 'my-shell-down)))
